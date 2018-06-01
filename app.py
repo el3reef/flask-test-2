6ac4111d69c65a0091cd7f4fed54f3234234e95e
Unknown <mo.elareef@gmail.com>
2018-06-01 18:33:31 +0200

    vfgv

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def homepage():
     print(score)
     xxx = "hoo"
 
-    return model.predict_proba(x)
+    return str(score[0])
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
